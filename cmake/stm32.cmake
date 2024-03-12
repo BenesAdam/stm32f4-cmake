51fdc207996a7405f39eba5490aa578bbeb82737
BenesAdam <adam.bilovice@seznam.cz>
2024-03-12 18:45:38 +0100

    libopencm3 with working debug paths

--- cmake/stm32.cmake
+++ cmake/stm32.cmake
@@ -1,5 +1,8 @@
 include(FetchContent)
 
+#==============================================================================
+# Clone libopencm3
+#==============================================================================
 set(LIBOPENCM3_GIT_URL "https://github.com/libopencm3/libopencm3")
 set(LIBOPENCM3_GIT_DESTINATION_DIR "${CMAKE_CURRENT_BINARY_DIR}/libopencm3")
 
@@ -24,20 +27,47 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(libopencm3)
 
-# create a target to build libopencm3 -- only for the target we need
-add_custom_target(libopencm3 
-  COMMAND wsl make TARGETS=stm32/f4
-  WORKING_DIRECTORY ${libopencm3_SOURCE_DIR}
-)
+#==============================================================================
+# Build libopencm3 .a file
+#==============================================================================
+set(libopencm3_library ${libopencm3_SOURCE_DIR}/lib/libopencm3_stm32f4.a)
 
-# Create a specific CPU target with the appropriate options etc
+if(NOT EXISTS ${libopencm3_library})
+  find_program(SH_EXECUTABLE NAMES sh.exe)
+  if(NOT SH_EXECUTABLE)
+    message(FATAL_ERROR "sh.exe not found.")
+  endif()
+
+  cmake_path(GET CMAKE_C_COMPILER PARENT_PATH CMAKE_C_COMPILER_DIR)
+  if(NOT CMAKE_C_COMPILER_DIR)
+    message(FATAL_ERROR "CMAKE_C_COMPILER is not set or invalid.")
+  endif()
+
+  find_program(PYTHON_EXECUTABLE NAMES python3.exe)
+  if(NOT PYTHON_EXECUTABLE)
+    message(FATAL_ERROR "python3.exe not found.")
+  endif()
+  get_filename_component(PYTHON_DIR ${PYTHON_EXECUTABLE} DIRECTORY)
+
+  execute_process(
+      COMMAND ${SH_EXECUTABLE} -c 'export PATH=\"${PYTHON_DIR}:${CMAKE_C_COMPILER_DIR}:\$PATH\" && make TARGETS=stm32/f4 CC=arm-none-eabi-gcc AR=arm-none-eabi-ar'
+      WORKING_DIRECTORY ${libopencm3_SOURCE_DIR}
+      RESULT_VARIABLE result
+  )
+  if(result)
+      message(FATAL_ERROR "Failed to build: ${libopencm3_SOURCE_DIR}")
+  endif()
+endif()
+
+#==============================================================================
+# Integrate libopencm3 into cmake
+#==============================================================================
 add_library(stm32 STATIC IMPORTED)
 set_property(TARGET stm32 PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libopencm3_SOURCE_DIR}/include)
-set_property(TARGET stm32 PROPERTY IMPORTED_LOCATION ${libopencm3_SOURCE_DIR}/lib/libopencm3_stm32f4.a)
-add_dependencies(stm32 libopencm3)
+set_property(TARGET stm32 PROPERTY IMPORTED_LOCATION ${libopencm3_library})
 target_link_directories(stm32 INTERFACE ${libopencm3_SOURCE_DIR}/lib)
 
-file(GLOB_RECURSE LIBOPENCM3_SOURCES "${libopencm3_SOURCE_DIR}/lib/*.c")
+file(GLOB_RECURSE LIBOPENCM3_SOURCES "${libopencm3_SOURCE_DIR}/lib/" "*.c")
 add_custom_target(libopencm3_sources SOURCES ${LIBOPENCM3_SOURCES})
 add_dependencies(stm32 libopencm3_sources)
 
--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,16 @@ extern "C"
 int main() {
   cSysTick::Setup();
 
+  cSysTick::Wait(3000);
+
   rcc_periph_clock_enable(RCC_LED_PORT);
   gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
 
   while(true)
   {
     gpio_set(LED_PORT, LED_PIN);
-    cSysTick::Wait(100);
+    cSysTick::Wait(300);
     gpio_clear(LED_PORT, LED_PIN);
-    cSysTick::Wait(4900);
+    cSysTick::Wait(300);
   }
 }
