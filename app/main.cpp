c26ee9776adec9c37de90da6f0b97122b6cd06a7
BenesAdam <adam.bilovice@seznam.cz>
2025-03-06 20:13:29 +0100

    45us pulse HIGH with 500Hz freq on PA0.

--- app/main.cpp
+++ app/main.cpp
@@ -2,210 +2,64 @@ extern "C"
 {
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
 }
 
 #include "systick.hpp"
-#include "i2c1.hpp"
-#include "display.hpp"
-#include "noinit_vars.hpp"
-#include "errorhandler_inst.hpp"
-#include "bitset.hpp"
 
-const ui64 MorseShortDelayTime = 150U; // milliseconds
-const ui64 MorseLongDelayTime = 3U * MorseShortDelayTime;
-const ui64 MorseEndOfLetterTime = (3U - 1U) * MorseShortDelayTime;
-const ui64 MorseEndOfWordTime = (7U - 1U) * MorseShortDelayTime;
-
-#define BOLD_QUESTIONMARK_SYMBOL eDisplayCharacter::CUSTOM_1
-#define LED_PORT GPIOC
-#define LED_PIN GPIO13
-
-void BlinkShort(void);
-void BlinkLong(void);
-void InitializeLight(void);
-void BlinkMorseCode(void);
-
-void InitializeDisplay(void);
-void HandleResets(void);
-void AskQuestion(void);
-void ShowTimer(void);
-
-cDisplay display;
+void InitTimers(void);
 
 int main(void)
 {
-  nsNoinitVars::ResetCount++;
-  nsNoinitVars::InitializeOnColdStart();
   cSysTick::Setup();
-  i2c1.Setup();
-  InitializeDisplay();
-  InitializeLight();
-
-  BlinkShort();
-  HandleResets();
-  AskQuestion();
+  InitTimers();
 
   while (true)
   {
-    ShowTimer();
-    ErrorHandler.Cyclic();
   }
 
   return 0;
 }
 
-void BlinkShort(void)
+extern "C" void tim2_isr(void)
 {
-  gpio_clear(LED_PORT, LED_PIN);
-  cSysTick::DelayMs(MorseShortDelayTime);
-  gpio_set(LED_PORT, LED_PIN);
-  cSysTick::DelayMs(MorseShortDelayTime);
-}
-
-void BlinkLong(void)
-{
-  gpio_clear(LED_PORT, LED_PIN);
-  cSysTick::DelayMs(MorseLongDelayTime);
-  gpio_set(LED_PORT, LED_PIN);
-  cSysTick::DelayMs(MorseShortDelayTime);
-}
-
-void InitializeLight(void)
-{
-  rcc_periph_clock_enable(RCC_GPIOC);
-
-#ifdef STM32F4
-  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
-#else
-  gpio_set_mode(LED_PORT, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, LED_PIN);
-#endif
-}
-
-void BlinkMorseCode(void)
-{
-  // S
-  BlinkShort();
-  BlinkShort();
-  BlinkShort();
-  cSysTick::DelayMs(MorseEndOfLetterTime);
-
-  // O
-  BlinkLong();
-  BlinkLong();
-  BlinkLong();
-  cSysTick::DelayMs(MorseEndOfLetterTime);
-
-  // S
-  BlinkShort();
-  BlinkShort();
-  BlinkShort();
-  cSysTick::DelayMs(MorseEndOfLetterTime);
-
-  // End of the word
-  cSysTick::DelayMs(MorseEndOfWordTime);
-}
-
-void InitializeDisplay(void)
-{
-  display.Init(0x27U);
-  const ui8 questionMarkBitMap[] =
-      {
-          0b11111,
-          0b11011,
-          0b10001,
-          0b00111,
-          0b00100,
-          0b00000,
-          0b01110,
-          0b01110,
-      };
-  display.SetCustomSymbol(BOLD_QUESTIONMARK_SYMBOL, questionMarkBitMap);
-  display.Clear();
-}
-
-void HandleResets(void)
-{
-  if (nsNoinitVars::ResetCount == 0U)
+  if (timer_get_flag(TIM2, TIM_SR_UIF))
   {
-    return;
+    timer_clear_flag(TIM2, TIM_SR_UIF);
+
+    timer_disable_counter(TIM5);
+    timer_generate_event(TIM5, TIM_EGR_UG);
+    timer_enable_counter(TIM5);
   }
-
-  // Print number of resets
-  display.Print(L"Reset No.: ");
-  display.Print(static_cast<ui32>(nsNoinitVars::ResetCount));
-  display.NewLine();
-
-  display.Print(L"0b");
-  cBitset<14> bitset(nsNoinitVars::ResetCount);
-  ui8 buffer[bitset.GetStringSize()];
-  bitset.ToString(buffer);
-
-  for(ui8 i = 0U; i < bitset.GetStringSize() - 1U; i++)
-  {
-    display.Print(static_cast<wchar_t>(buffer[i]));
-  }
-
-  // Continue...
-  const ui32 waitAfterResetCount = (nsNoinitVars::ResetCount > 1U) ? 1000U : 2000U;
-  cSysTick::DelayMs(waitAfterResetCount);
-  display.Clear();
-
-  if ((nsNoinitVars::ResetCount) % 3U == 0U)
-  {
-    display.Print(L"Are you mad");
-
-    for (ui8 i = 0U; i < 3U; i++)
-    {
-      display.Print(BOLD_QUESTIONMARK_SYMBOL);
-    }
-
-    display.NewLine();
-    display.Print(L"You are crazy...");
-
-    BlinkMorseCode();
-    BlinkMorseCode();
-    display.Clear();
-    display.Print(L"Anyway...");
-    BlinkMorseCode();
-  }
-  else
-  {
-    display.Print(L"Anyway...");
-    cSysTick::DelayMs(1000);
-  }
-
-  display.Clear();
 }
 
-void AskQuestion(void)
+void InitTimers(void)
 {
-  display.Print(L"How long can you\n");
-  display.Print(L"...not blink?");
-  const ui32 waitAfterQuestion = (nsNoinitVars::ResetCount == 0U) ? 3000U : 1000;
-  cSysTick::DelayMs(waitAfterQuestion);
-}
+  // TIM2 -> interrupt every 2ms (500Hz)
+  rcc_periph_clock_enable(RCC_TIM2);
+  timer_set_prescaler(TIM2, 95);
+  timer_set_period(TIM2, 1999);
+  timer_enable_irq(TIM2, TIM_DIER_UIE);
+  nvic_enable_irq(NVIC_TIM2_IRQ);
+  nvic_set_priority(NVIC_TIM2_IRQ, 2);
+  timer_enable_counter(TIM2);
 
-void ShowTimer(void)
-{
-  static ui64 startMeasuringTime = cSysTick::Millis();
-  static ui64 nextToggleTime = 0U;
+  // PA0 controlled by TIM5 CH1
+  rcc_periph_clock_enable(RCC_GPIOA);
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO0);
+  gpio_set_af(GPIOA, GPIO_AF2, GPIO0);
 
-  display.Clear();
-  display.Print(L"Blinkless for:\n");
+  // TIM -> generate 45us pulse
+  rcc_periph_clock_enable(RCC_TIM5);
 
-  ui64 currentTime = cSysTick::Millis() - startMeasuringTime;
-  ui32 seconds = currentTime / 1000ULL;
-  ui32 millis = currentTime % 1000ULL;
+  timer_disable_counter(TIM5);
+  timer_set_prescaler(TIM5, 95);
+  timer_set_period(TIM5, 45);
 
-  display.Print(seconds);
-  display.Print(eDisplayCharacter::PERIOD);
-  display.Print(millis);
+  timer_set_oc_mode(TIM5, TIM_OC1, TIM_OCM_PWM2);
+  timer_enable_oc_output(TIM5, TIM_OC1);
+  timer_set_oc_value(TIM5, TIM_OC1, 1);
 
-  if (currentTime >= nextToggleTime)
-  {
-    gpio_toggle(LED_PORT, LED_PIN);
-    nextToggleTime = currentTime + 1000ULL;
-  }
-
-  cSysTick::DelayMs(150);
+  timer_one_shot_mode(TIM5);
 }
